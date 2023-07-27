4b126882d67d8c665a4f4c3864e8583257a213b7
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-07-27 16:55:14 +0530

    fourth commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/"> Home </Link>
-        <Link to="/contact"> About </Link>
+        <Link to="/about"> About </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
