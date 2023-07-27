8b7b503db0ceb9c300c345b752fef641655b16d5
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-07-27 16:10:13 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
-import React from 'react';
-import './App.css';
-
-const productsArr = [
-  {
-    title: 'Colors',
-    price: 100,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-  },
-  {
-    title: 'Black and white Colors',
-    price: 50,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-  },
-  {
-    title: 'Yellow and Black Colors',
-    price: 70,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-  },
-  {
-    title: 'Blue Color',
-    price: 100,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-  },
-];
-
-const ProductCard = ({ product }) => {
-  return (
-    <div className="card">
-      <img src={product.imageUrl} className="card-img-top" alt={product.title} />
-      <div className="card-body">
-        <h5 className="card-title">{product.title}</h5>
-        <p className="card-text">Price: ${product.price}</p>
-      </div>
-    </div>
-  );
-};
-
-const ProductsScreen = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        {productsArr.map((product, index) => (
-          <div key={index} className="col-md-3">
-            <ProductCard product={product} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>E-commerce Products</h1>
-      </header>
-      <main>
-        <ProductsScreen />
-      </main>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,23 @@
+
+
+export const PRODUCTS = [
+ {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 68.0,
+    productImage: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 120.0,
+    productImage: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+  },
+  
+];
